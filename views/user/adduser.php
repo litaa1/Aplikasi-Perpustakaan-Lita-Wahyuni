dadeb0f51d9d89ef3d33212e571c5b446fa5db26
pepi <pepipanda63@gmail.com>
2022-03-10 13:48:29 +0700

    Aplikasi Perpustakaan Lita wahyuni

---
+++ views/user/adduser.php
@@ -0,0 +1,48 @@
+<div class="container-fluid">
+<div class="row justify-content-center">
+        <div class="col-lg-6">
+            <div class="card shadow mb-4">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <h4 align="center"><font color="black" face="fantasy">Tambah Data user</font></h4>
+                        <br>
+                        <form action="model/user/prosesadduser.php" method="POST">
+                            <table class="table">
+                            
+                                <tr>
+                                    <td>Id User</td>
+                                    <td>:</td>
+                                    <td><input type="text"  class="form-control" name="id_user" id="id_user" /></td>
+                                </tr>
+                                <tr>
+                                    <td>Id Pegawai</td>
+                                    <td>:</td>
+                                    <td><input type="text" class="form-control" name="id_pegawai" id="id_pegawai" /></td>
+                                </tr>
+                                <tr>
+                                    <td>User Name</td>
+                                    <td>:</td>
+                                    <td><input type="text" class="form-control" name="username" id="username" /></td>
+                                </tr>
+                                <tr>
+                                    <td>Password</td>
+                                    <td>:</td>
+                                    <td><input type="text"  class="form-control" name="password" id="password"></input></td>
+                                </tr>
+                                <tr>
+                                    <td>Level</td>
+                                    <td>:</td>
+                                    <td><input type="text"  class="form-control" name="level" id="level"></input></td>
+                                </tr>
+                                <td colspan="3">
+                                        <input type="submit" class="btn btn-info" value="Simpan">
+                                    </td>
+
+                            </table>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ views/user/user.php
@@ -0,0 +1,73 @@
+
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">USER</h1>
+                   
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-info">Tabel User
+                            <a href="beranda.php?page=user&aksi=add">
+                                <button class="btn btn-info">Tambah Data</button>
+                                </a>
+                            </h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Id User</th>
+                                            <th>Id Pegawai</th>
+                                            <th>User Name</th>
+                                            <th>Password</th>
+                                            <th>Level</th>
+                                            <th>aksi</th>
+                                            
+                                            
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                           <th>Id User</th>
+                                           <th>Id Pegawai</th>
+                                           <th>User Name</th>
+                                           <th>Password</th>
+                                           <th>Level</th>
+                                           <th>aksi</th>
+                                           
+                                        </tr>
+                                    </tfoot>
+<tbody>
+                                        <?php
+                                        $sql = "select * from tabel_user";
+                                        $show = $database->query($sql);
+
+                                        foreach($show as $user):
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $user['id_user']?></td>
+                                            <td><?php echo $user['id_pegawai']?></td>
+                                            <td><?php echo $user['username']?></td>
+                                            <td><?php echo $user['password']?></td>
+                                            <td><?php echo $user['level']?></td>
+                                            <td>
+                                            <a href="beranda.php?page=user&aksi=edit&id=<?php echo $user['id_user']?>"
+                                                class="btn-btn info btn-circle">
+                                                    <i class="fas fa-edit"></i>
+                                                    </a>
+                                                <a href="model/user/prosesdeleteuser.php?id=<?php echo $user['id_user']?>"
+                                                class="btn btn-danger btn-circle">
+                                                    <i class="fas fa-trash"></i>
+                                                    </a>
+                                            </a>
+                                            </td>
+                                        </tr>
+                                        <?php endforeach; ?>
+                                        </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
