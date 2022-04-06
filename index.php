d43c96d930358b60d0d76fad8f1fc2e95a5591fb
litaa1 <itaa2283@gmail.com>
2022-04-06 12:29:14 +0700

    perpustaan

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Aplikasi Perpustakaan</title>
+    <title>Aplikasi Perpustakaan lita</title>
 
     <!-- Custom fonts for this template-->  
     <link href="template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
